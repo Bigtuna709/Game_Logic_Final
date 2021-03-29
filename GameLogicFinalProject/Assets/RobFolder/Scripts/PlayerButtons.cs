a2311c9c4067db3ba80a503a563e48d895ea060e
Robert Day <rday8@my.centennialcollege.ca>
2021-03-29 14:41:53 -0400

    Added player movement
    added player movement

---
+++ GameLogicFinalProject/Assets/RobFolder/Scripts/PlayerButtons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerButtons : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    public bool IsPressed
+    {
+        get;
+        private set;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        IsPressed = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        IsPressed = false;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IsPressed = false;
+    }
+}
---
+++ GameLogicFinalProject/Assets/RobFolder/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerMovementSpeed;
+
+    public Rigidbody rb;
+
+    public PlayerButtons moveUpButton;
+    public PlayerButtons moveDownButton;
+    public PlayerButtons moveLeftButton;
+    public PlayerButtons moveRightButton;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = Vector3.zero;
+        if (moveUpButton.IsPressed)
+        {
+            velocity.z -= playerMovementSpeed;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+        }
+        if (moveDownButton.IsPressed)
+        {
+            velocity.z += playerMovementSpeed;
+            transform.rotation = Quaternion.LookRotation(-Vector3.forward);
+        }
+        if (moveLeftButton.IsPressed)
+        {
+            velocity.x += playerMovementSpeed;
+            transform.rotation = Quaternion.LookRotation(-Vector3.right);
+        }
+        if (moveRightButton.IsPressed)
+        {
+            velocity.x -= playerMovementSpeed;
+            transform.rotation = Quaternion.LookRotation(Vector3.right);
+        }
+
+        rb.velocity = velocity * Time.deltaTime;
+    }
+}
