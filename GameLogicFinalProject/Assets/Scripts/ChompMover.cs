2b478fe4c4f72b917fbfe306986916173fd49414
Robert Day <rday8@my.centennialcollege.ca>
2021-03-29 13:59:14 -0400

    updated project
    updated project

---
+++ GameLogicFinalProject/Assets/Scripts/ChompMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ChompMover : MonoBehaviour
+{
+    public Animator animator;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+    private void Update()
+    {
+        float speed = agent.velocity.magnitude;
+        animator.SetFloat("Speed", speed);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
+
