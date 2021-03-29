2d9c8f10833f72b33034b4c55491507b62c3b417
Robert Day <rday8@my.centennialcollege.ca>
2021-03-29 13:19:25 -0400

    did some edits
    edits

---
+++ Game_Logic_Final/Assets/RobFolder/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public PlayerButton moveUpButton;
+    public PlayerButton moveDownButton;
+    public PlayerButton moveLeftButton;
+    public PlayerButton moveRightButton;
+
+    private void Start()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+    private void Update()
+    {
+       // if(Input.GetMouseButtonDown(1))
+       // {
+       //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       //     RaycastHit hit;
+       //     if(Physics.Raycast(ray, out hit, 100))
+       //     {
+       //         playerNavMesh.SetDestination(hit.point);
+       //     }
+       // }
+    }
+    public void MoveUp()
+    {
+        playerNavMesh.Move(-Vector3.forward);
+    }
+}
