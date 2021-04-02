6c1d2a058318583f26c49110178ba5b7ff5a65dd
Jolanne <73859592+Jolanne@users.noreply.github.com>
2021-04-01 20:21:25 -0400

    Added Advertisements & Pause Menu
    - Added a Pause Menu into the game on TerryScene
    
    - Working Rewards Ads on JolanneScene
    
    -Button Assets made

---
+++ GameLogicFinalProject/Assets/JolanFolder/Scripts/Advertisements.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Advertisements : MonoBehaviour, IUnityAdsListener
+{
+    string gameID = "4073392";
+    bool testMode = true;
+
+    public string placementIdRewardVideo = "AOTDReward1";
+    public string placementIdBanner = "AOTDBanner1";
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+
+        Advertisement.Initialize(gameID, testMode);
+
+    }
+
+    public void ShowRewardVideo()
+    {
+        if(Advertisement.IsReady(placementIdRewardVideo))
+        {
+            Advertisement.Show(placementIdRewardVideo);
+        }
+    }
+    public void OnUnityAdsReady(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+
+    }
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if(showResult == ShowResult.Finished)
+        {
+            Debug.Log("You have successfully gained a single life.");
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            Debug.Log("Skipped the ad, unable to recieve reward");
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.Log("Failed to watch");
+        }
+    }
+
+    public void OnDestroy()
+    {
+        Advertisement.RemoveListener(this);
+    }
+
+}
---
+++ GameLogicFinalProject/Assets/JolanFolder/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    //Reference to button
+    public Button pauseMenuBTN;
+
+    //Reference to PauseMenuCanvas
+    public GameObject pauseMenuCanvas;
+
+    public static bool IsGamePaused = false;
+
+    public void PauseGame()
+    {
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+
+    }
+}
