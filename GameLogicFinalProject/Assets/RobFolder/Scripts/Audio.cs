7f828804183f4fab495a97679f56581964fa40f0
Robert Day <rday8@my.centennialcollege.ca>
2021-04-08 12:15:23 -0400

    Added audio
    added an audio manger and a background music

---
+++ GameLogicFinalProject/Assets/RobFolder/Scripts/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Audio
+{
+    public string name;
+    public AudioClip audioClip;
+
+    [Range(0f, 1f)]
+    public float pitch;
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+}
---
+++ GameLogicFinalProject/Assets/RobFolder/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public Audio[] audioData;
+    public AudioSource backGroundMusic;
+
+    private void Start()
+    {
+        foreach(Audio audio in audioData)
+        {
+            audio.audioSource = gameObject.AddComponent<AudioSource>();
+            audio.audioSource.clip = audio.audioClip;
+            audio.audioSource.volume = audio.volume;
+            audio.audioSource.pitch = audio.pitch;
+            audio.audioSource.loop = audio.loop;
+            audio.audioSource.playOnAwake = false;
+        }
+        backGroundMusic.Play();
+    }
+
+    public void PlayClip(string name)
+    {
+        Audio a = Array.Find(audioData, clip => clip.name == name);
+        if(a == null)
+        {
+            Debug.Log("<color=red>No such audioclip with that name!</color>");
+            return;
+        }
+        a.audioSource.Play();
+    }
+
+    public void StopCLip(string name)
+    {
+        Audio a = Array.Find(audioData, clip => clip.name == name);
+        if (a == null)
+        {
+            Debug.Log("<color=red>No such audioclip with that name!</color>");
+            return;
+        }
+        a.audioSource.Stop();
+    }
+}
