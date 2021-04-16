d3f6a960af11f789979158141349d84749635e6b
TerryTsePakLueng <terrytse22@gmail.com>
2021-04-15 20:19:17 -0400

    commenting trigger area
    commenting

--- GameLogicFinalProject/Assets/RobFolder/Scripts/GameOverTriggerArea.cs
+++ GameLogicFinalProject/Assets/RobFolder/Scripts/GameOverTriggerArea.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameOverTriggerArea : MonoBehaviour
 {
+    /// <summary>
+    /// End game trigger resets gamestate to Area1
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
